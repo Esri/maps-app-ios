fa5e8334a1cf42d760179871416e529c5f70ece8
Nicholas Furness <nfurness@nixta.com>
2017-03-24 13:54:13 -0400

    Initial Commit

---
+++ maps-app-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  maps-app-ios
+//
+//  Created by Nicholas Furness on 3/24/17.
+//  Copyright © 2017 Esri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
