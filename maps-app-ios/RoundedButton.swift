1d7469272b576d5be20f15ddf2f64184e711b377
Nicholas Furness <nfurness@nixta.com>
2017-06-18 18:11:24 -0400

    Rename and move some ViewController files

--- maps-app-ios/RoundedButton.swift
+++ maps-app-ios/RoundedButton.swift
@@ -46,35 +46,3 @@ public class RoundedButton: UIButton {
         layer.masksToBounds = layer.cornerRadius > 0
     }
 }
-
-@IBDesignable
-public class RoundedImageView: UIImageView {
-    @IBInspectable var cornerRadius:CGFloat = 0.0 {
-        didSet {
-            setRadius()
-        }
-    }
-    
-    @IBInspectable var borderWidth: CGFloat = 0.0 {
-        didSet {
-            layer.borderWidth = borderWidth
-        }
-    }
-    
-    @IBInspectable var borderColor: UIColor? {
-        didSet {
-            layer.borderColor = borderColor?.cgColor
-        }
-    }
-    
-    @IBInspectable var circular: Bool = false {
-        didSet {
-            setRadius()
-        }
-    }
-    
-    private func setRadius() {
-        layer.cornerRadius = circular ? self.bounds.width/2 : cornerRadius
-        layer.masksToBounds = layer.cornerRadius > 0
-    }
-}
---
+++ maps-app-ios/RoundedImageView.swift
@@ -0,0 +1,41 @@
+//
+//  RoundedImageView.swift
+//  maps-app-ios
+//
+//  Created by Nicholas Furness on 6/18/17.
+//  Copyright © 2017 Esri. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+public class RoundedImageView: UIImageView {
+    @IBInspectable var cornerRadius:CGFloat = 0.0 {
+        didSet {
+            setRadius()
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var circular: Bool = false {
+        didSet {
+            setRadius()
+        }
+    }
+    
+    private func setRadius() {
+        layer.cornerRadius = circular ? self.bounds.width/2 : cornerRadius
+        layer.masksToBounds = layer.cornerRadius > 0
+    }
+}
