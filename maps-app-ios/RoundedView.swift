9188bec72fdc0c7282c4a2b49863b9c1cbff7c0f
Nicholas Furness <nfurness@nixta.com>
2017-06-20 12:37:43 -0400

    Add a shadow to the control bar.

--- maps-app-ios/RoundedView.swift
+++ maps-app-ios/RoundedView.swift
@@ -12,7 +12,7 @@ import UIKit
 public class RoundedView: UIView {
     @IBInspectable var cornerRadius:CGFloat = 0.0 {
         didSet {
-            setRadius()
+            setLayerProperties()
         }
     }
     
@@ -30,12 +30,30 @@ public class RoundedView: UIView {
     
     @IBInspectable var circular: Bool = false {
         didSet {
-            setRadius()
+            setLayerProperties()
         }
     }
     
-    private func setRadius() {
+    @IBInspectable var shadow:Bool = false {
+        didSet {
+            setLayerProperties()
+        }
+    }
+    
+    private func setLayerProperties() {
         layer.cornerRadius = circular ? self.bounds.width/2 : cornerRadius
-        layer.masksToBounds = layer.cornerRadius > 0
+        
+        if shadow {
+            layer.shadowColor = UIColor.black.cgColor
+            layer.shadowOpacity = 0.3
+            layer.shadowRadius = 3
+            layer.shadowOffset = CGSize(width: 0, height: 0)
+            layer.masksToBounds = false
+        } else {
+            layer.shadowColor = UIColor.clear.cgColor
+            layer.shadowOpacity = 1
+            layer.shadowRadius = 0
+            layer.masksToBounds = layer.cornerRadius > 0
+        }
     }
 }
