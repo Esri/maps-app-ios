dd5e9628edd0372c531fa6c172f6f2aeb28c1e1b
Nicholas Furness <nfurness@nixta.com>
2017-07-08 17:15:48 -0400

    BasemapDisplayViewController refactoring

--- maps-app-ios/Basemaps View/BasemapsDisplayViewController.swift
+++ maps-app-ios/Basemaps View/BasemapsDisplayViewController.swift
@@ -11,29 +11,11 @@ import UIKit
 import UIKit
 import ArcGIS
 
-class BasemapsDisplayViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class BasemapsDisplayViewController: UIViewController {
     var items:[AGSPortalItem] {
         get {
             return mapsAppState.basemaps
         }
-    }
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return items.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BasemapItem", for: indexPath)
-        
-        if let cell = cell as? BasemapCollectionCell, indexPath.row <= items.count {
-            cell.item = items[indexPath.row]
-        }
-        
-        return cell
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -44,29 +26,7 @@ class BasemapsDisplayViewController: UIViewController, UICollectionViewDataSourc
         mapsAppState.currentItem = (sender as? PortalItemCollectionCell)?.item
     }
     
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        mapsAppState.currentBasemap = items[indexPath.row]
-        
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    
     @IBAction func cancelBasemapPicker(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
-    }
-}
-
-
-extension BasemapsDisplayViewController: UICollectionViewDataSourcePrefetching {
-    // Remember to write up the prefetchDelegtate in the Storyboard
-    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
-        for indexPath in indexPaths {
-            let item = items[indexPath.row]
-            item.thumbnail?.load() { error in
-                if let error = error {
-                    print("Error pre-fetching basemap at index \(indexPath.row): \(error.localizedDescription)")
-                }
-            }
-        }
     }
 }
