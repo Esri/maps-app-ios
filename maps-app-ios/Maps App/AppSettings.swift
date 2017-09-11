0b7bcaefdecb8c51263c75c5db6dbc897cd52733
Nicholas Furness <nfurness@nixta.com>
2017-09-10 20:07:21 -0400

    Properly read ClientID

--- maps-app-ios/Maps App/AppSettings.swift
+++ maps-app-ios/Maps App/AppSettings.swift
@@ -14,10 +14,7 @@ struct AppSettings {
         return (agsSettings[name] as? T)
     }
 
-    private static let authSettings:[String:Any] = getAgsSetting(named: "PortalAuthConfig") ?? [String:Any]()
-    private static func getAuthSetting<T>(named name:String) -> T? {
-        return (authSettings[name] as? T)
-    }
+    private static let authSettings:[String:Any]? = getAgsSetting(named: "PortalAuthConfig")
 
     
     // MARK: Runtime Licensing
@@ -31,7 +28,7 @@ struct AppSettings {
     // If there is a PortalURL setting in the info.plist file, then use that, otherwise leave it blank and the
     // app will fall back to ArcGIS Online.
     static let portalURL:URL? = {
-        if let urlString:String = getAuthSetting(named: "PortalURL"), let url = URL(string: urlString) {
+        if let urlString = authSettings?["PortalURL"] as? String, let url = URL(string: urlString) {
             return url
         }
         return nil
@@ -40,7 +37,7 @@ struct AppSettings {
     // Set up AppClientID in the project's info.plist. This is used for the OAuth panel and will determine what app users see
     // when they log in to authorize the app to view their account and use their routing/geocoding tasks. A ClientID is specific
     // to a particular portal (and is derived from an Application created within that portal).
-    static let clientID = getAuthSetting(named: "ClientID") ?? ""
+    static let clientID = authSettings?["ClientID"] as? String ?? ""
 
 
     // MARK: Runtime OAuth Configuration
