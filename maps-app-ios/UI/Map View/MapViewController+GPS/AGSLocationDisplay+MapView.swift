1d865716bd8b2bd5e3e9a202025276bcc171b7b1
Eli Gregory <egregory@esri.com>
2019-04-05 10:49:34 -0700

    migrates to swift5 using Xcode migration tool

--- maps-app-ios/UI/Map View/MapViewController+GPS/AGSLocationDisplay+MapView.swift
+++ maps-app-ios/UI/Map View/MapViewController+GPS/AGSLocationDisplay+MapView.swift
@@ -23,7 +23,7 @@ private var autoPanSequence:[AGSLocationDisplayAutoPanMode] = [
 extension AGSLocationDisplayAutoPanMode {
     func next() -> AGSLocationDisplayAutoPanMode {
         // Cycle through the AutoPan modes this app wants to use...
-        let newIndex = ((autoPanSequence.index(of: self) ?? -1) + 1) % autoPanSequence.count
+        let newIndex = ((autoPanSequence.firstIndex(of: self) ?? -1) + 1) % autoPanSequence.count
         return autoPanSequence[newIndex]
     }
 }
