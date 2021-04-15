d5c3df9f767e5b27fbb7e7d41ece72fd0ddf8eb7
Eli Gregory <egregory@esri.com>
2021-04-15 16:14:46 -0700

    removes deprecated AGSApplicationDelegate method.

--- maps-app-ios/Maps App/Maps App Delegate/MapsAppDelegate+OAuthLogin.swift
+++ maps-app-ios/Maps App/Maps App Delegate/MapsAppDelegate+OAuthLogin.swift
@@ -15,7 +15,6 @@
 import ArcGIS
 
 extension MapsAppDelegate {
-    
     // MARK: OAuth
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         // Handle OAuth callback from application(app,url,options) when the app's URL schema is called.
@@ -23,18 +22,10 @@ extension MapsAppDelegate {
         // See also AppSettings and AppContext.setupAndLoadPortal() to see how the AGSPortal is configured
         // to handle OAuth and call back to this application.
         if let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false),
-            urlComponents.scheme == AppSettings.appSchema, urlComponents.host == AppSettings.authURLPath {
-            
-            // Pass the OAuth callback through to the ArcGIS Runtime helper function
-            AGSApplicationDelegate.shared().application(app, open: url, options: options)
-            
-            // See if we were called back with confirmation that we're authorized.
-            if let _ = urlComponents.queryParameter(named: "code") {
-                // If we were authenticated, there should now be a shared credential to use. Let's try it.
-                mapsAppContext.signInCurrentPortalIfPossible()
-            }
+           urlComponents.scheme == AppSettings.appSchema, urlComponents.host == AppSettings.authURLPath {
+            // If we were authenticated, there should now be a shared credential to use. Let's try it.
+            mapsAppContext.signInCurrentPortalIfPossible()
         }
         return true
     }
-    
 }
